d1ddec216bbe1f9bf2f0df4ad190129f1fb0ec85
Diogo Correia <me@diogotc.com>
2020-12-10 11:05:57 +0000

    Add tests boilerplate

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const fs = require("fs").promises;
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+const app = express();
+const path = require("path");
+const spawn = require("child_process").spawn;
+
+app.use(bodyParser.text());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/run", async (req, res) => {
+  const uuid = uuidv4();
+  const filePath = path.join(__dirname, "tests", `runner-${uuid}.py`);
+  await fs.writeFile(filePath, req.body);
+
+  const pythonProcess = spawn("python3", [
+    path.join(__dirname, "tests", "test.py"),
+    `runner-${uuid}`,
+  ]);
+
+  let result = "";
+
+  pythonProcess.stdout.on("data", (data) => {
+    result += data.toString() + "\n";
+  });
+
+  pythonProcess.stderr.on("data", (data) => {
+    result += data.toString() + "\n";
+  });
+
+  pythonProcess.on("close", async () => {
+    await fs.unlink(filePath);
+    res.send(result);
+  });
+
+  setTimeout(() => {
+    result += "Max timeout exceeded (3s). Program killed.";
+    pythonProcess.kill();
+  }, 3000);
+});
+
+app.listen(process.env.PORT || 5000);
+console.log(
+  `Listening on port ${
+    process.env.PORT || 5000
+  }. You can change this by passing a value to the PORT environment variable.`
+);
---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+import importlib
+import os
+from io import StringIO
+
+target = importlib.import_module(sys.argv[1])
+
+
+class SomeTest(unittest.TestCase):
+    def test_example(self):
+        """
+        Example test
+        """
+        result = target.some_function()
+        self.assertEqual(result, True)
+
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'])
