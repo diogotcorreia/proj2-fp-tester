4b6c06d2fc9c382185beb16b3e1970c660e72463
Luís Fonseca <72040432+luishfonseca@users.noreply.github.com>
2020-12-30 21:00:49 +0000

    Enqueue Tests (#13)

--- index.js
+++ index.js
@@ -10,68 +10,116 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 const timeout = 300;
 
-let processes = {};
-let timer = {};
+let queue = [];
+let sockets = {};
+let processing;
 
-const closeProcess = (socket, processes, timer) => {
-  if (processes[socket.id]) {
-    processes[socket.id].kill();
-    delete processes[socket.id];
-    socket.emit("done");
+const enqueue = (q, s, code, skipMocks) => {
+  sockets[s.id] = {"code": code, "skipMocks": skipMocks};
+  p = q.push(s);
+  s.emit(
+    "result",
+    `[Mooshak da Feira] Your test is in the queue. There ${
+      p == 1 ? "is 1 test" : `are ${p} tests`
+    } before yours.\n`
+  );
+}
+
+const notifyNewPos = q => {
+  q.forEach((s, i) => {
+    s.emit(
+      "result",
+      `There ${
+        i == 0 ? "is 1 test" : `are ${i + 1} tests`
+      } before yours.\n`
+    );
+  });
+}
+
+const next = q => {
+  s = q.shift();
+  notifyNewPos(q);
+  return s
+}
+
+const leaveQueue = (q, s) => {
+  i = q.indexOf(s);
+  if (i == -1) {
+    console.log('socket tried to leave queue but was not in it');
+    return;
   }
-  if (timer[socket.id]) {
-    clearTimeout(timer[socket.id]);
-    delete timer[socket.id];
+  delete sockets[s.id];
+  q.splice(i, 1);
+  notifyNewPos(q.slice(i));
+}
+
+const killProcess = s => {
+  if (sockets[s.id]) {
+    if (sockets[s.id]["timer"]) clearTimeout(sockets[s.id]["timer"]);
+    if (sockets[s.id]["process"]) sockets[s.id]["process"].kill();
+    delete sockets[s.id];
   }
+  s.emit("done");
+  processing = undefined;
 };
+
+const processTests = s => {
+  socket.emit(
+    "result",
+    `Executing tests... ${
+      sockets[s.id]["skipMocks"]
+        ? "(skipping abstraction tests)"
+        : `(testing abstraction; might take up to ${timeout / 60} minutes)`
+    }\n\n`
+  );
+
+  child = spawn("python3.5", [
+    "-u",
+    path.join(__dirname, "tests", "test.py"),
+    sockets[s.id]['skipMocks'] ? "False" : "True",
+  ]);
+  sockets[s.id]["process"] = child;
+
+  sockets[s.id]["timer"] = setTimeout(() => {
+    s.emit(
+      "result",
+      `Program killed because it exceeded timeout (${timeout}s) :(`
+    );
+    killProcess(s);
+  }, timeout * 1000);
+
+  result = (data) => socket.emit("result", data.toString());
+  child.stdout.on("data", result);
+  child.stderr.on("data", result);
+
+  child.stdin.write(sockets[s.id]['code']);
+  child.stdin.end();
+
+  child.on("close", () => killProcess(s));
+}
+
 
 io.on("connection", (socket) => {
   socket.on("submit", (code, skipMocks) => {
-    if (processes[socket.id]) closeProcess(socket, processes, timer);
-    socket.emit("clear");
-
-    socket.emit(
-      "result",
-      `[Mooshak da Feira] Executing tests... ${
-        skipMocks
-          ? "(skipping abstraction tests)"
-          : `(testing abstraction; might take up to ${timeout / 60} minutes)`
-      }\n\n`
-    );
-
-    child = spawn("python3.5", [
-      "-u",
-      path.join(__dirname, "tests", "test.py"),
-      skipMocks ? "False" : "True",
-    ]);
-    processes[socket.id] = child;
-
-    result = (data) => socket.emit("result", data.toString());
-    child.stdout.on("data", result);
-    child.stderr.on("data", result);
-
-    child.stdin.write(code);
-    child.stdin.end();
-
-    child.on("close", () => closeProcess(socket, processes, timer));
-
-    timer[socket.id] = setTimeout(() => {
-      socket.emit(
-        "result",
-        `Program killed because it exceeded timeout (${timeout}s) :(`
-      );
-      closeProcess(socket, processes, timer);
-    }, timeout * 1000);
+      socket.emit("clear");
+      enqueue(queue, socket, code, skipMocks);
   });
 
   socket.on("disconnect", () => {
-    if (processes[socket.id]) closeProcess(socket, processes, timer);
+    if (processing == socket.id) {
+      killProcess(socket);
+    } else if (queue.indexOf(socket) != -1) {
+      leaveQueue(queue, socket)
+    }
   });
 
   socket.on("kill", () => {
-    if (processes[socket.id]) {
+    if (processing == socket.id) {
       socket.emit("result", "\nProgram killed by user :(");
-      closeProcess(socket, processes, timer);
+      killProcess(socket);
+    } else if (queue.indexOf(socket) != -1) {
+      socket.emit("result", "\nLeft queue.");
+      leaveQueue(queue, socket);
     }
   });
 });
@@ -82,3 +130,11 @@ console.log(
     process.env.PORT || 5000
   }. You can change this by passing a value to the PORT environment variable.`
 );
+
+setInterval(() => {
+  if (!processing && queue.length != 0) {
+    socket = next(queue);
+    processing = socket.id;
+    processTests(socket);
+  }
+}, 10);
